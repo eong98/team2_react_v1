4695c8c4e9a0f953046e7030e590d15b15c364db
chan <dhaprk999@gmail.com>
2026-08-10 16:36:59 +0900

    Delete team2_react_v1 directory

--- team2_react_v1/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './styles/normalize.css' /* 브라우저 스타일 초기화 */
-import './index.css' /* 커스텀 전역 스타일 */
-
-import App from './routes/App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
